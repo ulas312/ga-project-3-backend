9632d4aa86bd38a290b3ec9e33234da4f30afb68
wyndhams <wyn.roy@hotmail.co.uk>
2023-01-04 10:31:57 +0000

    Changes to workoutsController and router - added following functionality: create new workout, get single workout, update/delete workout (admin only)

--- config/router.js
+++ config/router.js
@@ -1,16 +1,29 @@
 import express from 'express';
+import muscleGroupController from '../controller/muscleGroupController.js';
 import workoutsController from '../controller/workoutsController.js';
 import userController from '../controller/userController.js';
+import secureRoute from '../middleware/secureRoute.js';
 
 const Router = express.Router();
 
-Router.route('/workouts').get(workoutsController.getAllMuscleGroups);
+Router.route('/workouts')
+  .get(workoutsController.getAllWorkouts)
+  .post(secureRoute, workoutsController.createNewWorkout);
 
 // Router.route('/workouts/search').get(workoutsController.searchWorkouts)
 
-Router.route('/workouts/:id').get(workoutsController.getWorkoutsByMuscleGroup);
+Router.route('/workouts/:id')
+  .get(workoutsController.getSingleWorkout)
+  .put(secureRoute, workoutsController.updateSingleWorkout)
+  .delete(secureRoute, workoutsController.deleteWorkout);
 
-Router.route('/log-workout').get(workoutsController.getWorkoutLog);
+Router.route('/muscle-group').get(muscleGroupController.getAllMuscleGroups);
+
+Router.route('/muscle-group/:id/workouts').get(
+  muscleGroupController.getWorkoutsByMuscleGroup
+);
+  
+// Router.route('/log-workout').get(workoutsController.getWorkoutLog);
 
 // Router.route('/account').get(userController.getUserAccount);
 
