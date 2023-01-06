eb59050ceb50d73cc9542cba7accdca0d25010f4
Ebrar Hassan <ebrarhassan93@gmail.com>
2023-01-06 10:29:39 +0000

    seed changes

--- db/seed.js
+++ db/seed.js
@@ -16,7 +16,56 @@ const NON_ADMIN_USER = {
   email: 'notanadmin@email.com',
 };
 
-const chestShouldersTriceps = [
+const abs = [
+  {
+    name: 'Bicycle Crunch',
+    image:
+      'https://weighttraining.guide/wp-content/uploads/2016/11/bicycle-crunch-resized-1.png?ezimgfmt=ng%3Awebp%2Fngcb4',
+    description:
+      'Raise your knees to a 90-degree angle and alternate extending your legs as if pedaling a bike. Twist your body to touch your elbow to the opposite knee with each pedal motion. With proper form, bicycle crunches can increase core strength while adding a cardio element to your ab exercise routine.',
+    reps: 10,
+    sets: 3,
+    rest: 30,
+    difficulty: 'Beginner',
+    totalTime: 10,
+    caloriesBurned: 200,
+    muscleGroup: 'Abs',
+    equipmentRequired: 'None',
+  },
+  {
+    name: 'Plank',
+    image:
+      'https://weighttraining.guide/wp-content/uploads/2021/06/Weighted-front-plank.png',
+    description:
+      'The plank is a bodyweight exercise which involves holding the trunk part of your body in a straight line off the ground. The static exercise engages multiple muscle groups at the same time which makes it extremely effective at strengthening your core, whilst also working the shoulders, arms and glutes.',
+    reps: 0,
+    sets: 3,
+    rest: 30,
+    difficulty: 'Intermediate',
+    totalTime: 5,
+    caloriesBurned: 180,
+    muscleGroup: 'Abs',
+    equipmentRequired: 'None',
+  },
+
+  {
+    name: 'Sandbag sit up',
+    image:
+      'https://www.inspireusafoundation.org/wp-content/uploads/2022/05/sit-up-1024x344.png',
+    description:
+      'Hold a sandbag with both hands and extend your arms above you. Tense your core as you lift your torso up so your upper body forms a V shape with your thighs. Lower under control back to the start position.',
+    reps: 12,
+    sets: 4,
+    rest: 30,
+    difficulty: 'Intermediate',
+    totalTime: 15,
+    caloriesBurned: 240,
+    muscleGroup: 'Abs',
+    equipmentRequired: 'Sandbag',
+  },
+];
+
+const chest = [
   {
     name: 'Incline Bench Press',
     image:
@@ -34,19 +83,18 @@ const chestShouldersTriceps = [
   },
 
   {
-    name: 'Bent Over Rows',
-    image:
-      'https://static.vecteezy.com/system/resources/previews/008/259/297/original/man-doing-dumbbell-bent-over-rows-exercise-flat-illustration-isolated-on-white-background-free-vector.jpg',
+    name: 'Pushup',
+    image: 'https://fitnessvolt.com/wp-content/uploads/2019/04/push-up.jpg',
     description:
-      'Bending your knees slightly, and your core tight, bend over at the waist keeping your lower back tight. Bending over until your upper body is at a 45-degree bend or lower, pull the bar up towards your lower chest. Keep your elbows as close to your sides as possible',
-    reps: 10,
+      'Pushups are an exercise in which a person, keeping a prone position, with the hands palms down under the shoulders, the balls of the feet on the ground, and the back straight, pushes the body up and lets it down by an alternate straightening and bending of the arms.',
+    reps: 12,
     sets: 3,
     rest: 30,
     difficulty: 'Intermediate',
     totalTime: 10,
-    caloriesBurned: 200,
-    muscleGroup: 'Upper back (latissimus dorsi, rhomboids, and trapezius)',
-    equipmentRequired: 'Barbell or Dumbbell',
+    caloriesBurned: 250,
+    muscleGroup: 'Chest',
+    equipmentRequired: 'None',
   },
 
   {
@@ -66,7 +114,7 @@ const chestShouldersTriceps = [
   },
 ];
 
-const backAndBiceps = [
+const back = [
   {
     name: 'Deadlift',
     image:
@@ -79,24 +127,24 @@ const backAndBiceps = [
     difficulty: 'Intermediate',
     totalTime: 15,
     caloriesBurned: 220,
-    muscleGroup: 'Glutes, Hamstrings and Trapezius',
+    muscleGroup: 'Glutes, Hamstrings, Back and Trapezius',
     equipmentRequired: 'Barbell',
   },
 
   {
-    name: 'Barbell Curl',
+    name: 'Back Extension',
     image:
-      'http://cdn.shopify.com/s/files/1/1497/9682/articles/2_73c5a35b-aa8c-45ff-8ef1-8d074ed9c16d.jpg?v=1648742110',
+      'https://cdn.shopify.com/s/files/1/1633/7705/files/hyperextensions_480x480.jpg?v=1628477529',
     description:
-      'A barbell curl is a variation of the biceps curl that uses a weighted barbell. Perform barbell curls by grabbing a barbell with a shoulder-width supinated grip (palms facing towards your body). Hinge your elbows, and lift the barbell toward your chest.',
-    reps: 10,
+      'Back extension is a type of stabilization exercise used in back rehabilitation programs that involves bending the spine backwards.',
+    reps: 12,
     sets: 3,
     rest: 30,
     difficulty: 'Beginner',
-    totalTime: 8,
+    totalTime: 15,
     caloriesBurned: 250,
-    muscleGroup: 'Biceps',
-    equipmentRequired: 'Barbell',
+    muscleGroup: 'Back',
+    equipmentRequired: 'Back extension machine',
   },
 
   {
@@ -114,9 +162,24 @@ const backAndBiceps = [
     muscleGroup: 'Upper Trapezius',
     equipmentRequired: 'Barbell',
   },
+  {
+    name: 'Bent Over Rows',
+    image:
+      'https://static.vecteezy.com/system/resources/previews/008/259/297/original/man-doing-dumbbell-bent-over-rows-exercise-flat-illustration-isolated-on-white-background-free-vector.jpg',
+    description:
+      'Bending your knees slightly, and your core tight, bend over at the waist keeping your lower back tight. Bending over until your upper body is at a 45-degree bend or lower, pull the bar up towards your lower chest. Keep your elbows as close to your sides as possible',
+    reps: 10,
+    sets: 3,
+    rest: 30,
+    difficulty: 'Intermediate',
+    totalTime: 10,
+    caloriesBurned: 200,
+    muscleGroup: 'Upper back (latissimus dorsi, rhomboids, and trapezius)',
+    equipmentRequired: 'Barbell or Dumbbell',
+  },
 ];
 
-const legsAndAbs = [
+const legs = [
   {
     name: 'Squats',
     image:
@@ -152,37 +215,37 @@ const legsAndAbs = [
   },
 
   {
-    name: 'Bicycle Crunch',
+    name: 'Leg Curl',
     image:
-      'https://weighttraining.guide/wp-content/uploads/2016/11/bicycle-crunch-resized-1.png?ezimgfmt=ng%3Awebp%2Fngcb4',
+      'https://weighttraining.guide/wp-content/uploads/2017/01/dumbbell-leg-curl-resized.png?ezimgfmt=ng%3Awebp%2Fngcb4',
     description:
-      'Raise your knees to a 90-degree angle and alternate extending your legs as if pedaling a bike. Twist your body to touch your elbow to the opposite knee with each pedal motion. With proper form, bicycle crunches can increase core strength while adding a cardio element to your ab exercise routine.',
-    reps: 10,
+      'The hamstring curl, also called a leg curl, is an exercise that strengthens the hamstrings. It involves bending your knees and moving your heels toward your butt while the rest of your body stays still. Typically, the exercise is done on a leg curl machine.',
+    reps: 12,
     sets: 3,
     rest: 30,
     difficulty: 'Beginner',
     totalTime: 10,
     caloriesBurned: 200,
-    muscleGroup: 'Abs',
-    equipmentRequired: 'None',
+    muscleGroup: 'Legs',
+    equipmentRequired: 'Leg curl machine',
   },
 ];
 
-const fullBody = [
+const shoulders = [
   {
-    name: 'Running',
+    name: 'Reverse Fly',
     image:
-      'https://www.mensjournal.com/wp-content/uploads/mf/1280-treadmill.jpg?w=1200&h=960&crop=1&quality=86&strip=all',
+      'https://weighttraining.guide/wp-content/uploads/2016/12/seated-bent-over-lateral-raise-resized.png?ezimgfmt=ng%3Awebp%2Fngcb4',
     description:
-      'Running is a method of terrestrial locomotion allowing humans and other animals to move rapidly on foot. Running is a type of gait characterized by an aerial phase in which all feet are above the ground (though there are exceptions).',
-    reps: 0,
-    sets: 0,
-    rest: 90,
-    difficulty: 'Beginner',
+      'Lean forward, letting your arms hang down next to your calves with your elbows slightly bent. Slowly raise the weights until your elbows are level with your shoulders. Then slowly lower the weights to the starting position. You will feel tension in your shoulders and the muscles across your upper back.',
+    reps: 15,
+    sets: 3,
+    rest: 60,
+    difficulty: 'Intermediate',
     totalTime: 30,
     caloriesBurned: 250,
-    muscleGroup: 'Full body',
-    equipmentRequired: 'Treadmill',
+    muscleGroup: 'Shoulders',
+    equipmentRequired: 'Dumbbells',
   },
 
   {
@@ -199,6 +262,21 @@ const fullBody = [
     caloriesBurned: 300,
     muscleGroup: 'Full body',
     equipmentRequired: 'Cable Machine',
+  },
+  {
+    name: 'Lateral Raise',
+    image:
+      'https://weighttraining.guide/wp-content/uploads/2016/11/cable-one-arm-lateral-raise-resized.png',
+    description:
+      'A lateral raise is a strength training shoulder exercise characterized by lifting a pair of dumbbells away from your body in an external rotation. Lateral raises work the trapezius muscle in your upper back as well as the deltoid muscle group in your shoulders—particularly the anterior and lateral deltoids.',
+    reps: 10,
+    sets: 3,
+    rest: 30,
+    difficulty: 'Hard',
+    totalTime: 10,
+    caloriesBurned: 300,
+    muscleGroup: 'Shoulders',
+    equipmentRequired: 'Dumbbells',
   },
 ];
 
@@ -220,78 +298,94 @@ async function seedDb() {
   const [user, adminUser] = await User.create([NON_ADMIN_USER, ADMIN_USER]);
   console.log('🏋🏽‍♀️🤸🏼‍♀️Created admin user with id', user._id);
 
-  const chestShouldersTricepsGroup = await Muscle.create({
-    name: 'Chest, Shoulders & Triceps',
+  const absGroup = await Muscle.create({
+    name: 'Abs',
   });
-  console.log('🏋🏽‍♀️🤸🏼‍♀️Created muscle group chestShouldersTriceps');
+  console.log('🏋🏽‍♀️🤸🏼‍♀️Created muscle group Abs');
 
-  const updatedChestShouldersTriceps = chestShouldersTriceps.map((workout) => ({
+  const updatedAbs = abs.map((workout) => ({
     ...workout,
     addedBy: adminUser._id,
-    muscleGroup: chestShouldersTricepsGroup._id,
+    muscleGroup: abs._id,
   }));
 
-  const chestShouldersTricepsFromDb = await Workouts.create(
-    updatedChestShouldersTriceps
-  );
+  const absFromDb = await Workouts.create(updatedAbs);
 
   await Muscle.findOneAndUpdate(
-    { _id: chestShouldersTricepsGroup._id },
-    { $push: { groups: chestShouldersTricepsFromDb.map((b) => b._id) } }
+    { _id: absGroup._id },
+    { $push: { groups: absFromDb.map((b) => b._id) } }
   );
 
-  const backAndBicepsGroup = await Muscle.create({
-    name: 'Back & Biceps',
+  const chestGroup = await Muscle.create({
+    name: 'Chest',
   });
-  console.log('🏋🏽‍♀️🤸🏼‍♀️Created muscle group', backAndBiceps);
+  console.log('🏋🏽‍♀️🤸🏼‍♀️Created muscle group', chest);
 
-  const updatedBackAndBiceps = backAndBiceps.map((workout) => ({
+  const updatedChest = chest.map((workout) => ({
     ...workout,
     addedBy: adminUser._id,
-    muscleGroup: backAndBicepsGroup._id,
+    muscleGroup: chestGroup._id,
   }));
 
-  const backAndBicepsFromDb = await Workouts.create(updatedBackAndBiceps);
+  const chestFromDb = await Workouts.create(updatedChest);
 
   await Muscle.findOneAndUpdate(
-    { _id: backAndBicepsGroup._id },
-    { $push: { groups: backAndBicepsFromDb.map((b) => b._id) } }
+    { _id: chestGroup._id },
+    { $push: { groups: chestFromDb.map((b) => b._id) } }
   );
 
-  const legsAndAbsGroup = await Muscle.create({
-    name: 'Legs & Abs',
+  const legsGroup = await Muscle.create({
+    name: 'Legs',
   });
-  console.log('🏋🏽‍♀️🤸🏼‍♀️Created muscle group', legsAndAbs);
+  console.log('🏋🏽‍♀️🤸🏼‍♀️Created muscle group', legs);
 
-  const updatedLegsAndAbs = legsAndAbs.map((workout) => ({
+  const updatedLegs = legs.map((workout) => ({
     ...workout,
     addedBy: adminUser._id,
-    muscleGroup: legsAndAbsGroup._id,
+    muscleGroup: legsGroup._id,
   }));
 
-  const legsAndAbsFromDb = await Workouts.create(updatedLegsAndAbs);
+  const legsFromDb = await Workouts.create(updatedLegs);
 
   await Muscle.findOneAndUpdate(
-    { _id: legsAndAbsGroup._id },
-    { $push: { groups: legsAndAbsFromDb.map((b) => b._id) } }
+    { _id: legsGroup._id },
+    { $push: { groups: legsFromDb.map((b) => b._id) } }
   );
 
-  const fullBodyGroup = await Muscle.create({
-    name: 'Full Body',
+  const backGroup = await Muscle.create({
+    name: 'Back',
   });
-  console.log('🏋🏽‍♀️🤸🏼‍♀️Created muscle group', fullBody);
+  console.log('🏋🏽‍♀️🤸🏼‍♀️Created muscle group', back);
 
-  const updatedFullBody = fullBody.map((workout) => ({
+  const updatedBack = back.map((workout) => ({
     ...workout,
     addedBy: adminUser._id,
-    muscleGroup: fullBodyGroup._id,
+    muscleGroup: backGroup._id,
   }));
 
-  const fullBodyFromDb = await Workouts.create(updatedFullBody);
+  const backFromDb = await Workouts.create(updatedBack);
 
   await Muscle.findOneAndUpdate(
-    { _id: fullBodyGroup._id },
-    { $push: { groups: fullBodyFromDb.map((b) => b._id) } }
+    { _id: backGroup._id },
+    { $push: { groups: backFromDb.map((b) => b._id) } }
+  );
+
+  const shouldersGroup = await Muscle.create({
+    name: 'Shoulders',
+  });
+  console.log('🏋🏽‍♀️🤸🏼‍♀️Created muscle group', shoulders);
+
+  const updatedShoulders = shoulders.map((workout) => ({
+    ...workout,
+    addedBy: adminUser._id,
+    muscleGroup: shouldersGroup._id,
+  }));
+
+  const shouldersFromDb = await Workouts.create(updatedShoulders);
+
+  await Muscle.findOneAndUpdate(
+    { _id: shouldersGroup._id },
+    { $push: { groups: shouldersFromDb.map((b) => b._id) } }
   );
 
   await disconnectDb();
